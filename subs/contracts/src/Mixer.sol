04038b73556c031bfaa998f86b570b2f425897b8
crypt0 <m374-crypt0@pm.me>
2026-01-05 08:26:29 +0000

    feat: untested deposit stub

--- subs/contracts/src/Mixer.sol
+++ subs/contracts/src/Mixer.sol
@@ -4,7 +4,14 @@ pragma solidity 0.8.30;
 interface IVerifier { }
 
 contract Mixer {
+  uint256 public constant DENOMINATION = 0.001 ether;
+
   IVerifier public immutable verifier;
+
+  mapping(bytes32 => bool) public commitments;
+
+  error CommitmentAlreadyExists();
+  error WrongDepositValue();
 
   /// @notice Deploy this Mixer contract
   /// @param verifier_ A Verifier smart contract able to verify zk proofs
@@ -15,11 +22,11 @@ contract Mixer {
   /// @notice deposit funds into the Mixer
   /// @param commitment_ the poseidon commitment of the secret and the
   ///        nullifier computed off-chain
-  function deposit(bytes32 commitment_) external {
-    // - ensure the user send the correct fixed amount of ETH (denomination)
-    // - check if commitment_ has already been used (prevent double deposit)
-    // - add the commitment_ to a datastructure containing all commitments
-    // -
+  function deposit(bytes32 commitment_) external payable {
+    require(!commitments[commitment_], CommitmentAlreadyExists());
+    require(msg.value == DENOMINATION, WrongDepositValue());
+
+    commitments[commitment_] = true;
   }
 
   /// @notice Withdraw funds from the Mixer in way that protect privacy
